2b1a72411d8a4d15b7fa85846fc1fdc54c3237f0
Doruk Özgen <140007919+dorukozgen@users.noreply.github.com>
2024-04-22 02:08:15 +0300

    feat: Add helper methods

---
+++ src/test/java/utilities/ReusableMethods.java
@@ -0,0 +1,12 @@
+package utilities;
+
+public class ReusableMethods {
+
+    public static void sleep(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
